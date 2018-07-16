f5174fd00c971d1ac76394e0761a5d6f893f4db7
Xemsssa <Xemsss@gmail.com>
2018-07-16 17:28:04 +0300

    some  modification in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-// input range change color
+const val = 120;
 
-$('input[type="range"]').on("change mousemove", function () {
-    // alert("move");
-    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+let input_number = $('#size');
+let input_range = $('#range_size');
+input_range.on("change mousemove",  updateValue);
 
+function updateValue() {
+
+    input_number.val($(this).val());
+
+    let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
 
     $(this).css('background-image',
                 '-webkit-gradient(linear, left top, right top, '
@@ -11,4 +16,17 @@ $('input[type="range"]').on("change mousemove", function () {
                 + 'color-stop(' + val + ', #fff)'
                 + ')'
                 );
-});
+}
+
+
+//old one
+// $('input[type="range"]').on("change mousemove", function () {
+//     let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+
+//     $(this).css('background-image',
+//                 '-webkit-gradient(linear, left top, right top, '
+//                 + 'color-stop(' + val + ', #3588D3), '
+//                 + 'color-stop(' + val + ', #fff)'
+//                 + ')'
+//                 );
+// });
