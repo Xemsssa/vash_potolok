beda9a935571463b4dcdcda45cf233eb95752787
Xemsssa <Xemsss@gmail.com>
2018-07-17 11:24:31 +0300

    calculator form input range and number change value sync

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ const val = 120;
 const input_number = $('#size');
 const input_range = $('#range_size');
 input_range.on("change mousemove",  updateValue);
+input_number.on("change", function() {
+    input_range.val($(this).val());
+
+    let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+
+    input_range.css('background-image',
+                '-webkit-gradient(linear, left top, right top, '
+                + 'color-stop(' + val + ', #3588D3), '
+                + 'color-stop(' + val + ', #fff)'
+                + ')'
+                );
+})
 // $('#range_size').on("change mousemove",  function() {
 //     updateValue(input_number); 
 // });
