2bb8f19c41761b10ddd138791b61732b6b52f000
Xemsssa <Xemsss@gmail.com>
2018-07-17 14:29:16 +0300

    optimization and some small changes

--- js/main.js
+++ js/main.js
@@ -1,52 +1,77 @@
-const val = 120;
+$(document).ready(function (){
+    // const val = 120;
 
-const input_number = $('#size');
-const input_range = $('#range_size');
-input_range.on("change mousemove",  updateValue);
-input_number.on("change", function() {
-    input_range.val($(this).val());
-
-    let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
-
-    input_range.css('background-image',
-                '-webkit-gradient(linear, left top, right top, '
-                + 'color-stop(' + val + ', #3588D3), '
-                + 'color-stop(' + val + ', #fff)'
-                + ')'
-                );
-})
-
-function updateValue2 (one, two) {
-    console.log(one.val());
-    console.log(two.val());
-    // return two.val(one.val());
-}
-
-// $('#range_size').on("change mousemove",  function() {
-//     updateValue(input_number); 
-// });
-
-// function updateValue(params) {
+    // locate element
+    const input_number = $('#size');
+    const input_range = $('#range_size');
     
+    // add clickListener
+    input_range.on("change mousemove",  updateValue);
+    input_number.on("change", function() {
+        input_range.val($(this).val());
+
+        let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+
+        input_range.css('background-image',
+                    '-webkit-gradient(linear, left top, right top, '
+                    + 'color-stop(' + val + ', #3588D3), '
+                    + 'color-stop(' + val + ', #fff)'
+                    + ')'
+                    );
+    })
+
+    function updateValue() {
+        // synchronize
+        input_number.val($(this).val());
+
+        // set color in range depends on value
+        let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+
+        $(this).css('background-image',
+                    '-webkit-gradient(linear, left top, right top, '
+                    + 'color-stop(' + val + ', #3588D3), '
+                    + 'color-stop(' + val + ', #fff)'
+                    + ')'
+                    );
+    }
+});
+
+// const val = 120;
+
+// const input_number = $('#size');
+// const input_range = $('#range_size');
+// input_range.on("change mousemove",  updateValue);
+// input_number.on("change", function() {
+//     input_range.val($(this).val());
+
+//     let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+
+//     input_range.css('background-image',
+//                 '-webkit-gradient(linear, left top, right top, '
+//                 + 'color-stop(' + val + ', #3588D3), '
+//                 + 'color-stop(' + val + ', #fff)'
+//                 + ')'
+//                 );
+// })
+
+// function updateValue2 (one, two) {
+//     console.log(one.val());
+//     console.log(two.val());
+//     // return two.val(one.val());
 // }
 
-function updateValue() {
+// // $('#range_size').on("change mousemove",  function() {
+// //     updateValue(input_number); 
+// // });
 
-    input_number.val($(this).val());
+// // function updateValue(params) {
+    
+// // }
 
-    let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+// function updateValue() {
 
-    $(this).css('background-image',
-                '-webkit-gradient(linear, left top, right top, '
-                + 'color-stop(' + val + ', #3588D3), '
-                + 'color-stop(' + val + ', #fff)'
-                + ')'
-                );
-}
+//     input_number.val($(this).val());
 
-
-//old one
-// $('input[type="range"]').on("change mousemove", function () {
 //     let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
 
 //     $(this).css('background-image',
@@ -55,4 +80,17 @@ function updateValue() {
 //                 + 'color-stop(' + val + ', #fff)'
 //                 + ')'
 //                 );
-// });
+// }
+
+
+// //old one
+// // $('input[type="range"]').on("change mousemove", function () {
+// //     let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+
+// //     $(this).css('background-image',
+// //                 '-webkit-gradient(linear, left top, right top, '
+// //                 + 'color-stop(' + val + ', #3588D3), '
+// //                 + 'color-stop(' + val + ', #fff)'
+// //                 + ')'
+// //                 );
+// // });
