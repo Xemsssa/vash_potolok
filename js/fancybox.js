b4b8cf55a43515be4ea7ad10faa5047e38430304
Xemsssa <Xemsss@gmail.com>
2018-07-19 15:29:12 +0300

    zzz

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,23 +1,28 @@
 $(document).ready(function() {
+    
     	/* This is basic - uses default settings */
-	
-	$("a.our_work_example").fancybox();
+    // $("a.our_work_example").fancybox();
+	// $("a.our_work_example").attr('rel', 'group1').fancybox();
 	
 	/* Using custom settings */
 	
 	$("a.our_work_example").fancybox({
-		'hideOnContentClick': true
+        'hideOnContentClick':   true,
+        // 'hideOnContentClick':	false,
+        'titleShow':            false,
+        'showNavArrows':        true,
+        'enableEscapeButton':   true
 	});
 
 	/* Apply fancybox to multiple items */
 	
-	$("a.group").fancybox({
-		'transitionIn'	:	'elastic',
-		'transitionOut'	:	'elastic',
-		'speedIn'		:	600, 
-		'speedOut'		:	200, 
-		'overlayShow'	:	false
-    });
+	// $("a.our_work_example").fancybox({
+	// 	'transitionIn'	:	'elastic',
+	// 	'transitionOut'	:	'elastic',
+	// 	'speedIn'		:	600, 
+	// 	'speedOut'		:	200, 
+	// 	'overlayShow'	:	false
+    // });
     
     
 })
