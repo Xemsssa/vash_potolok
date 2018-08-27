5c73c1fc3ad77f04db03142f8f050868ad6e3f98
Xemsssa <Xemsss@gmail.com>
2018-08-27 17:13:01 +0300

    solve conflict with version

--- js/calculator.js
+++ js/calculator.js
@@ -1,10 +1,12 @@
 
-;$(document).ready(function () {
+$(document).ready(function () {
     const val = 120;
 
     const input_number = $('#size');
     const input_range = $('#range_size');
+
     input_range.on("change mousemove",  updateValue);
+    
     input_number.on("change", function() {
         input_range.val($(this).val());
 
@@ -34,32 +36,3 @@
     }
 })
 
-
-
-// function updateValue2 (one, two) {
-//     console.log(one.val());
-//     console.log(two.val());
-//     // return two.val(one.val());
-// }
-
-// $('#range_size').on("change mousemove",  function() {
-//     updateValue(input_number); 
-// });
-
-// function updateValue(params) {
-    
-// }
-
-
-
-//old one
-// $('input[type="range"]').on("change mousemove", function () {
-//     let val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
-
-//     $(this).css('background-image',
-//                 '-webkit-gradient(linear, left top, right top, '
-//                 + 'color-stop(' + val + ', #3588D3), '
-//                 + 'color-stop(' + val + ', #fff)'
-//                 + ')'
-//                 );
-// });
--- js/fancybox.js
+++ js/fancybox.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-    
-    	/* This is basic - uses default settings */
-    // $("a.our_work_example").fancybox();
-	// $("a.our_work_example").attr('rel', 'group1').fancybox();
-	
-	/* Using custom settings */
 	
 	$("a.our_work_example").fancybox({
         'hideOnContentClick':   true,
