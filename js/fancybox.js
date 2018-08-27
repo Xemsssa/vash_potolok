fc9c9cb46f818fa4a5759a86be8d69b62195ef03
Xemsssa <Xemsss@gmail.com>
2018-08-27 16:29:40 +0300

    -modal window -change png to svg -and etc.

--- js/fancybox.js
+++ js/fancybox.js
@@ -14,6 +14,25 @@ $(document).ready(function() {
         'enableEscapeButton':   true
 	});
 
+	$("#request").click(function() {
+		$.fancybox({
+			type: "inline",
+			content: "#request_form", 
+			'hideOnContentClick': false,
+			'enableEscapeButton': false,
+			'overlayShow': true,
+			'autoScale': true,
+			'autoDimensions': true,
+			'autoSize': true,
+			'showCloseButton' : false,
+			afterShow : function() {
+				$('.fancybox-skin').append('<a title="Close" class="fancybox-item fancybox-close" href="javascript:jQuery.fancybox.close();"></a>');
+   			}
+     
+
+		});
+	    });
+
 	/* Apply fancybox to multiple items */
 	
 	// $("a.our_work_example").fancybox({
@@ -23,7 +42,5 @@ $(document).ready(function() {
 	// 	'speedOut'		:	200, 
 	// 	'overlayShow'	:	false
     // });
-    
-    
 })
 
